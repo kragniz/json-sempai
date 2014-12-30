36c97aea9d3ea143f6a494c5f436ad7c0392cd6a
Louis Taylor <kragniz@gmail.com>
2014-12-30 03:54:22 +0000

    Add the import hook to sys.meta_path

--- jsonsempai.py
+++ jsonsempai.py
@@ -1,14 +1,17 @@
+import os
 import sys
 
 class SempaiLoader(object):
-    def __init__(self, *args):
-        print args
 
-    def find_module(self, fullname, path=None):
-        print 'finding', fullname, path
-        if fullname == 'simple':
-            return self
+    def find_module(self, name, path=None):
+        for d in sys.path:
+            json_path = os.path.join(d, '{}.json'.format(name))
+            if os.path.isfile(json_path):
+                print json_path
+                return self
         return None
 
-sys.path_hooks.append(SempaiLoader)
-sys.path.insert(0, 'simple')
+    def load_module(self, name):
+        raise ImportError('Hey, yo fool')
+
+sys.meta_path.append(SempaiLoader())
