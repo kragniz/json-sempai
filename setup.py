f5ec7859329c69517c6cfc44e6a8e7ca1d9a5faa
Louis Taylor <kragniz@gmail.com>
2014-12-31 18:34:33 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst') as file_readme:
+    readme = file_readme.read()
+
+setup(name='json-sepmai',
+      version='0.1.0',
+      description='Use json files with the import statement',
+      long_description=readme,
+      author='Louis Taylor',
+      author_email='kragniz@gmail.com',
+      license='MIT',
+      url='https://github.com/kragniz/json-sempai',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords='please don\'t use this library for anything',
+      py_modules= ['jsonsempai'],
+      test_suite='test_sempai',
+)
