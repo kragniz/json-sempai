bdf617ecb4a1465ce5d392c65d98165e6652c7e2
Louis Taylor <kragniz@gmail.com>
2015-01-01 00:36:09 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as file_readme:
     readme = file_readme.read()
 
 setup(name='json-sempai',
-      version='0.1.0',
+      version='0.2.0',
       description='Use json files with the import statement',
       long_description=readme,
       author='Louis Taylor',
