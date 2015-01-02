77a6d7c2da324463ce582e3a02c58fa8dc6e46ba
Louis Taylor <kragniz@gmail.com>
2015-01-02 17:43:55 +0000

    Use unittest instead of nose syntax

--- tests/test_sempai.py
+++ tests/test_sempai.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import sys
 import tempfile
+import unittest
 
 import jsonsempai
 
@@ -22,73 +23,75 @@ TEST_FILE = '''{
 }'''
 
 
-class TestSempai(object):
+class TestSempai(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.direc = tempfile.mkdtemp(prefix='jsonsempai')
         sys.path.append(self.direc)
 
         with open(os.path.join(self.direc, 'sempai.json'), 'w') as f:
             f.write(TEST_FILE)
 
-    def teardown(self):
+    def tearDown(self):
         sys.path.remove(self.direc)
         shutil.rmtree(self.direc)
 
     def test_import(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai
+        self.assertTrue(sempai is not None)
 
     def test_access(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.three == 3
+        self.assertEqual(3, sempai.three)
 
     def test_access_nested(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.one.two.three == 3
+        self.assertEqual(3, sempai.one.two.three)
 
     def test_acts_like_dict(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.one.two == {"three": 3}
+        self.assertEqual({"three": 3}, sempai.one.two)
 
     def test_set(self):
         with jsonsempai.imports():
             import sempai
         sempai.one.two.three = 4
-        assert sempai.one.two.three == 4
+        self.assertEqual(4, sempai.one.two.three)
 
     def test_del(self):
         with jsonsempai.imports():
             import sempai
         del sempai.one.two.three
-        assert sempai.one.two.get('three', 'not at home') == 'not at home'
+        self.assertEqual('not at home',
+                         sempai.one.two.get('three', 'not at home'))
 
     def test_location(self):
         del sys.modules['sempai'] # force the module to be reloaded
         with jsonsempai.imports():
             import sempai
-        assert sempai.__file__ == os.path.join(self.direc, 'sempai.json')
+        self.assertEqual(os.path.join(self.direc, 'sempai.json'),
+                         sempai.__file__)
 
     def test_array(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.array[0] == {"nested": "but dotted"}
-
+        self.assertEqual({"nested": "but dotted"},
+                         sempai.array[0])
     def test_array_dotting(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.array[0].nested == "but dotted"
+        self.assertEqual('but dotted', sempai.array[0].nested)
 
     def test_array_nested_dotting(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.array[1].array[0].and_nested_again == "dotted"
+        self.assertEqual('dotted', sempai.array[1].array[0].and_nested_again)
 
     def test_obj_in_list_in_list(self):
         with jsonsempai.imports():
             import sempai
-        assert sempai.lots_of_lists[0][0].in_da_list == True
+        self.assertTrue(sempai.lots_of_lists[0][0].in_da_list)
