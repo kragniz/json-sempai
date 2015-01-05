384b89380dce71a26a817b9b60511fdeb48c056b
im-rodrigo <rodrigopala91@gmail.com>
2015-01-04 17:53:58 -0800

    limits loader to relative json imports

--- jsonsempai/sempai.py
+++ jsonsempai/sempai.py
@@ -38,7 +38,44 @@ class SempaiLoader(object):
         mod.__loader__ = self
 
         decoder = json.JSONDecoder(object_hook=DottedDict)
-        
+
+        try:
+            with open(self.json_path) as f:
+                d = decoder.decode(f.read())
+        except ValueError:
+            raise ImportError(
+                '"{name}" does not contain valid json.'.format(name=self.json_path))
+        except:
+            raise ImportError(
+                'Could not open "{name}".'.format(name=self.json_path))
+
+        mod.__dict__.update(d)
+
+        sys.modules[name] = mod
+        return mod
+
+
+class SempaiContextLoader(object):
+    def __init__(self, json_path):
+        self.json_path = json_path
+
+    @classmethod
+    def find_module(cls, name, path=None):
+        for d, _, _ in os.walk(os.path.abspath(os.curdir)):
+            json_path = os.path.join(d, '{name}.json'.format(name=name))
+            if os.path.isfile(json_path):
+                return cls(json_path)
+
+    def load_module(self, name):
+        if name in sys.modules:
+            return sys.modules[name]
+
+        mod = imp.new_module(name)
+        mod.__file__ = self.json_path
+        mod.__loader__ = self
+
+        decoder = json.JSONDecoder(object_hook=DottedDict)
+
         try:
             with open(self.json_path) as f:
                 d = decoder.decode(f.read())
@@ -62,3 +99,4 @@ def imports():
         yield
     finally:
         sys.meta_path.remove(SempaiLoader)
+
